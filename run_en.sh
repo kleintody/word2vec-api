476ee6b559d3ae27f02e169510aec04bdb1af2a6
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-06-02 15:16:16 +0200

    fix helper scripts

--- run_en.sh
+++ run_en.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 model=${1:-${HOME}/data/Embeddings/en/wiki.en.vec.bz2}
-python2 word2vec-api.py --model ${model} --binary BINARY --port 6000
+python2 word2vec-api.py --model ${model} --port 6000
